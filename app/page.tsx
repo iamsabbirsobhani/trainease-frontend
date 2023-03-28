9c2b4fa20f287793b65ac68ce9db416003708caf
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-03-28 23:25:17 +0600

    add global layout metadata

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,10 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className="">
-      <DestinationForm />
-    </main>
+    <>
+      <main className="">
+        <DestinationForm />
+      </main>
+    </>
   );
 }
