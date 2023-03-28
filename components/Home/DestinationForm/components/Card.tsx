5a9ffabc8c23322e09a71d017a565f05f4b83639
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-03-28 22:55:21 +0600

    primereact, destinationform updated

--- components/Home/DestinationForm/components/Card.tsx
+++ components/Home/DestinationForm/components/Card.tsx
@@ -2,7 +2,7 @@ import Form from './Form';
 
 export default function Card() {
   return (
-    <div className=" shadow-xl	 rounded-sm w-[500px] mt-9 m-auto p-3 ">
+    <div className="shadow-md rounded-md w-[700px] mt-9 m-auto p-3">
       <Form />
     </div>
   );
--- components/Home/DestinationForm/components/Form.tsx
+++ components/Home/DestinationForm/components/Form.tsx
@@ -1,22 +1,112 @@
+'use client';
+
+import { MultiSelect, MultiSelectChangeEvent } from 'primereact/multiselect';
+import React, { useState } from 'react';
+import stationData from '@/public/stations.json';
+import { Calendar } from 'primereact/calendar';
+
+// interface for the station also can be null
+interface Station {
+  id: number;
+  name: string;
+  info?: string;
+}
+
 export default function Form() {
+  const stations: Station[] = stationData.map((station: any) => ({
+    id: station.id,
+    name: station.name,
+    info: station.info,
+  }));
+
+  const [selectedFromCities, setSelectedFromCities] = useState<Station[]>();
+  const [selectedToCities, setSelectedToCities] = useState<Station[]>();
+  const [citiesFrom, setCitiesFrom] = useState(stations);
+  const [citiesTo, setCitiesTo] = useState(stations);
+  const [date, setDate] = useState<any>();
+
+  const handleFromCities = (e: MultiSelectChangeEvent) => {
+    setSelectedFromCities(e.value);
+
+    if (e.value && e.value.length > 0) {
+      const filteredCities = stations.filter(
+        (city) => (city && city.name) !== (e.value && e.value[0].name),
+      );
+      setCitiesTo(filteredCities);
+    } else {
+      setCitiesTo(stations);
+    }
+  };
+
+  const handleToCities = (e: MultiSelectChangeEvent) => {
+    setSelectedToCities(e.value);
+
+    if (e.value && e.value.length > 0) {
+      const filteredCities = stations.filter(
+        (city) => (city && city.name) !== (e.value && e.value[0].name),
+      );
+      setCitiesFrom(filteredCities);
+    } else {
+      setCitiesFrom(stations);
+    }
+  };
+
+  const handleDate = (e: any) => {
+    setDate(e.value);
+    console.log(e.value);
+  };
+
   return (
     <div className="">
       <form action="">
         <div className=" flex justify-between">
-          <div className="from flex flex-col w-[200px]">
-            <label htmlFor="from">From</label>
-            <input className=" outline-none rounded-sm p-2 border-[1px] border-gray-500" type="text" name="from" id="from" />
+          <div className="from flex flex-col w-[300px]">
+            <label htmlFor="from" className=" text-lg font-semibold">
+              From
+            </label>
+            <MultiSelect
+              name="from"
+              value={selectedFromCities}
+              onChange={(e) => handleFromCities(e)}
+              options={citiesFrom}
+              optionLabel="name"
+              filter
+              placeholder="Select Cities"
+              maxSelectedLabels={1}
+              selectionLimit={1}
+              className="w-full md:w-20rem"
+            />
           </div>
 
-          <div className="to flex flex-col w-[200px]">
-            <label htmlFor="to">To</label>
-            <input className=" outline-none rounded-sm p-2 border-[1px] border-gray-500" type="text" name="to" id="to" />
+          <div className="to flex flex-col w-[300px]">
+            <label htmlFor="to" className="text-lg font-semibold">
+              To
+            </label>
+            <MultiSelect
+              name="to"
+              value={selectedToCities}
+              onChange={(e) => handleToCities(e)}
+              options={citiesTo}
+              optionLabel="name"
+              filter
+              placeholder="Select Cities"
+              maxSelectedLabels={1}
+              selectionLimit={1}
+              className="w-full md:w-20rem"
+            />
           </div>
         </div>
 
-        <div className="date flex flex-col w-[200px] mt-2">
-          <label htmlFor="date">Date</label>
-          <input className="outline-none rounded-sm p-2 border-[1px] border-gray-500" type="date" name="date" id="date" />
+        <div className="date flex flex-col w-full mt-2">
+          <label htmlFor="date" className="text-lg font-semibold">
+            Date
+          </label>
+          <Calendar
+            name="date"
+            value={date}
+            onChange={(e) => handleDate(e)}
+            showIcon
+          />
         </div>
       </form>
     </div>
