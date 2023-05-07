cbf6601bd3f8f0e14b041a04789d6a23bb235de7
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-05-07 09:25:17 +0600

    shift-home added

--- components/Navbar/DesktopNav/components/Navbar.tsx
+++ components/Navbar/DesktopNav/components/Navbar.tsx
@@ -7,14 +7,16 @@ export default function Navbar() {
     <>
       <div className=" flex justify-between items-center p-5 bg-gray-50 ">
         <div>
-          <Image
-            src="https://firebasestorage.googleapis.com/v0/b/general-eadd6.appspot.com/o/trainease%2Ftrainease-logo_1.png?alt=media&token=a921799d-ad95-4956-ace5-d5baba9591c4"
-            alt="Picture of the author"
-            width={250}
-            height={40}
-            className=" relative left-5"
-            blurDataURL="https://firebasestorage.googleapis.com/v0/b/general-eadd6.appspot.com/o/trainease%2Ftrainease-logo_1.png?alt=media&token=a921799d-ad95-4956-ace5-d5baba9591c4"
-          />
+          <Link href="/">
+            <Image
+              src="https://firebasestorage.googleapis.com/v0/b/general-eadd6.appspot.com/o/trainease%2Ftrainease-logo_1.png?alt=media&token=a921799d-ad95-4956-ace5-d5baba9591c4"
+              alt="Picture of the author"
+              width={250}
+              height={40}
+              className=" relative left-5"
+              blurDataURL="https://firebasestorage.googleapis.com/v0/b/general-eadd6.appspot.com/o/trainease%2Ftrainease-logo_1.png?alt=media&token=a921799d-ad95-4956-ace5-d5baba9591c4"
+            />
+          </Link>
         </div>
 
         <div className=" flex justify-between relative right-5">
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { DestinationForm } from '@/components/Home/DestinationForm';
 import Head from 'next/head';
-import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>TrainEase - Book Train Tickets Online</title>
+        <title>Search Tickets | TrainEase - Book Train Tickets Online</title>
         <meta
           name="description"
           content="Book train tickets online with TrainEase - the easiest and fastest way to buy train tickets."
@@ -61,27 +60,8 @@ export default function Home() {
         <meta property="og:image:type" content="image/png" />
       </Head>
 
-      <main className=" ">
-        <div className="max-w-5xl flex justify-between m-auto mt-10">
-          <div className="home-text w-1/2">
-            <h1 className=" text-5xl">Plan Your Next Train Journey</h1>
-            <p className="text-2xl">
-              <span>Search for Routes&nbsp;</span>
-              <span>Check Availability and Book Your Tickets Online</span>
-            </p>
-            <Link href="/search"> Search Ticket</Link>
-          </div>
-          <div className="home-bg w-1/2">
-            <Image
-              src="https://firebasestorage.googleapis.com/v0/b/general-eadd6.appspot.com/o/trainease%2Ftrain-bg.jpg?alt=media&token=35f96e04-32bc-48c6-917c-16e1c05d420f"
-              alt="Picture of the author"
-              width={500}
-              height={500}
-              className=" rounded-md"
-              blurDataURL="https://firebasestorage.googleapis.com/v0/b/general-eadd6.appspot.com/o/trainease%2Ftrain-bg.jpg?alt=media&token=35f96e04-32bc-48c6-917c-16e1c05d420f"
-            />
-          </div>
-        </div>
+      <main className="">
+        <DestinationForm />
       </main>
     </>
   );
